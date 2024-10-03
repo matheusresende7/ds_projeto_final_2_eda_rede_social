ac841834c78414a770f3e07121952ce438ff2d5d
Matheus Resende <75645023+matheusresende7@users.noreply.github.com>
2024-10-02 21:39:33 -0300

    v0.1

---
+++ params/consts.py
@@ -0,0 +1,23 @@
+# Data
+DATASET_RAW = '../data/dataset_raw.csv'
+DATASET_LEAN = '../data/dataset_lean.csv'
+DATASET_DUMMIES = '../data/dataset_dummies.csv'
+
+# Reports
+EDA_0 = '../reports/eda_0.html'
+EDA_1 = '../reports/eda_1.html'
+
+# Values
+RANDOM_STATE = 42
+
+# Images Outputs
+OUTPUT_SCHEDULE = '../images/output_schedule.png'
+
+# Images Illustrations
+WALLPAPER = '../images/wallpaper.png'
+BOXPLOT = '../images/boxplot.png'
+SCATTERPLOT = '../images/scatterplot.png'
+HISTPLOT = '../images/histplot.png'
+KDEPLOT = '../images/kdeplot.png'
+HEATMAP = '../images/heatmap.png'
+PAIRPLOTS = '../images/pairplots.png'
